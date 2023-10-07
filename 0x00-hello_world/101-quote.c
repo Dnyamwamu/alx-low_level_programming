5c1f3608c493222f0ddc654943cc305a739c8963
dnyamwamu <dnyamwamu@gmail.com>
2023-10-06 19:36:37 -0700

    Quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -12,7 +12,8 @@
  */
 int main(void)
 {
-	const char *msg = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	const char *msg = "and that piece of art is useful\"
+	- Dora Korpar, 2015-10-19\n";
 	write(STDERR_FILENO, msg, 59);
 	return (1);
 }
