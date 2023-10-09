6b7211c90603ca17934d415c003066f52b532ded
dnyamwamu <dnyamwamu@gmail.com>
2023-10-08 18:03:25 -0700

    Print Alphabt

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the App
+ *
+ * Description: Writes a program that alphabet in lowercase
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char letter = 'a';
+
+	while (letter <= 'z')
+	 {
+		if (letter != 'e' && letter != 'q')
+		{
+			putchar(letter);
+		}
+		letter++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
