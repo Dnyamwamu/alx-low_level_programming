4808e86162d0aada1e617cb8a599e61b21ee6d96
dnyamwamu <dnyamwamu@gmail.com>
2023-10-12 13:14:32 -0700

    Alphabet in Lowercase

---
+++ 0x02-functions_nested_loops/1-main.h
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - Entry point of the app
+ *
+ * Description: This program will print the lowercase
+ * alphabet folloewed by a newline
+ *
+ * Returns: Always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
