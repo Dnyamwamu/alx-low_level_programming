c752406e75506dd33bd57f9819ef748ecc0bd425
dnyamwamu <dnyamwamu@gmail.com>
2023-10-12 15:07:30 -0700

    Alphabet in lowercase

---
+++ 0x02-functions_nested_loops/1-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * main - entry point
+ *
+ * Description: Calls a function from 1-alphabet.c
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
