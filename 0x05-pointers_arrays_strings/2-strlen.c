a7535470e5ae951e061ea0d11f93d6e9983cd36e
dnyamwamu <dnyamwamu@gmail.com>
2023-10-17 14:42:48 -0700

    Strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+/**
+ *  * _strlen - Returns the length of a string.
+ * @s: A pointer to a string.
+ *
+ * This function calculates and returns the length of the input string.
+ * The length is determined by counting the characters in 
+ * the string until the null-terminating character '\0' is encountered.
+ *
+ * Return: The length of the string.
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s != '\0')
+	{
+		length++;
+		s++;
+	}
+
+	return length;
+}
