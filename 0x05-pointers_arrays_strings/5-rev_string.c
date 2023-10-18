7f2cc008a3d89455f68c3eb3ac776362aa44434d
dnyamwamu <dnyamwamu@gmail.com>
2023-10-17 19:39:44 -0700

    Rev string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * rev_string - Reverses a string.
+ * @s: A pointer to a string.
+ */
+void rev_string(char *s)
+{
+	int length = 0;
+	int start = 0;
+	char temp;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	while (start < length - 1)
+	{
+		temp = s[start];
+		s[start] = s[length - 1];
+		s[length - 1] = temp;
+		start++;
+		length--;
+	}
+}
