6b9db17b4be75d304e966e59ac504a71b226ddf3
dnyamwamu <dnyamwamu@gmail.com>
2023-10-17 19:12:11 -0700

    Puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-void reset_to_98(int *n);
-void swap_int(int *a, int *b);
+#include <stdio.h>
+
+/* Function to calculate the length of a string */
 int _strlen(char *s);
-void _puts(char *str)
+
+/* Function to print a string followed by a new line */
+void _puts(char *str);
+
+/* Function to update the value pointed to by a pointer to 98 */
+void reset_to_98(int *n);
+
+/* Function to swap the values of two integers */
+void swap_int(int *a, int *b);
 
 #endif /* MAIN_H */
