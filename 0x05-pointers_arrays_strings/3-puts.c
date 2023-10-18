2855729dc69c502a2685db7546d7b09549cdd110
dnyamwamu <dnyamwamu@gmail.com>
2023-10-17 19:19:03 -0700

    Puts

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -4,7 +4,8 @@
  * _puts - Prints a string followed by a new line to stdout.
  * @str: A pointer to a string.
  *
- * This function prints the input string to the standard output followed by a new line.
+ * This function prints the input string to the
+ * standard output followed by a new line.
  */
 void _puts(char *str)
 {
