1323fc1a4d1cf5da2b4767fe828d009ec739432c
dnyamwamu <dnyamwamu@gmail.com>
2023-10-20 17:29:09 -0700

    Reverse Array

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+/**
+ * reverse_array - Reverse the content of an array of integers
+ * @a: The array of integers to be reversed
+ * @n: The number of elements in the array
+ */
+void reverse_array(int *a, int n)
+{
+	int temp;
+	int i, j;
+
+	for (i = 0, j = n - 1; i < j; i++, j--)
+	{
+		temp = a[i];
+		a[i] = a[j];
+		a[j] = temp;
+	}
+}
