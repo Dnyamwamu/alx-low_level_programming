9ea0aa852868be3e7debe3fe1728f42a6889521c
dnyamwamu <dnyamwamu@gmail.com>
2023-10-22 03:29:08 -0700

    Times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * times_table - Prints the 9 times table starting from 0.
+ */
+void times_table(void)
+{
+	int i, j, product;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			product = i * j;
+			if (j == 0)
+				printf("%2d", product);
+			else
+				printf(",%3d", product);
+		}
+		printf("\n");
+	}
+}
