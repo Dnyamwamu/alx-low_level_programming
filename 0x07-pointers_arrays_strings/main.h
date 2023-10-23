11ab75f2e4ab79ddd4bbf959fd81ea306dd0dd62
dnyamwamu <dnyamwamu@gmail.com>
2023-10-23 08:26:36 -0700

    Memset

---
+++ 0x07-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * _memset - Fills memory with a constant byte.
+ * @s: A pointer to the memory area to be filled.
+ * @b: The constant byte to fill the memory with.
+ * @n: The number of bytes to fill.
+ *
+ * Return: A pointer to the memory area 's'.
+ */
+char *_memset(char *s, char b, unsigned int n);
+
+#endif /* MAIN_H */
