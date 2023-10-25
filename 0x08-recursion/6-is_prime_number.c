7585e6b3aea42fbd1afe18b862edfc5ee49ac035
dnyamwamu <dnyamwamu@gmail.com>
2023-10-25 11:08:29 -0700

    Is prime number

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,4 +1,35 @@
 /**
+ * is_prime_helper - It's a helper
+ * @n: The integer to check for primality.
+ * @divisor: The integer to check divisor
+ *
+ * Return: 1 if n is a prime number, 0 otherwise.
+ */
+int is_prime_helper(int n, int divisor)
+{
+	if (n <= 1)
+	{
+		return (0);
+	}
+	else if (divisor <= 2)
+	{
+		return (1);
+	}
+	else if (n % divisor == 0)
+	{
+		return (0);
+	}
+	else if (divisor * divisor > n)
+	{
+		return (1);
+	}
+	else
+	{
+		return (is_prime_helper(n, divisor - 1));
+	}
+}
+
+/**
  * is_prime_number - Check if an integer is a prime number.
  * @n: The integer to check for primality.
  *
@@ -6,30 +37,5 @@
  */
 int is_prime_number(int n)
 {
-	if (n <= 1)
-	{
-		return (0);
-	}
-	else if (n <= 3)
-	{
-		return (1);
-	}
-	else if (n % 2 == 0 || n % 3 == 0)
-	{
-		return (0);
-	}
-	else
-	{
-		int i = 5;
-
-		while (i * i <= n)
-		{
-			if (n % i == 0 || n % (i + 2) == 0)
-			{
-				return (0);
-			}
-			i += 6;
-		}
-		return (1);
-	}
+	return (is_prime_helper(n, n - 1));
 }
