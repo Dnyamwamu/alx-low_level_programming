f933bb11f00ddcc42fde3f9a15490036cfc50111
dnyamwamu <dnyamwamu@gmail.com>
2023-10-25 10:14:23 -0700

    Factorial

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+/**
+ * factorial - Calculate the factorial of a number.
+ * @n: The number for which to calculate the factorial.
+ *
+ * Return: The factorial of the number, or -1 for errors.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0 || n == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (n * factorial(n - 1));
+	}
+}
