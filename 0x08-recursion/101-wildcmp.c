4184d4d1e18d5650eb0e70a07336929cbedb257b
dnyamwamu <dnyamwamu@gmail.com>
2023-10-26 05:00:28 -0700

    Wildcamp

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -1,5 +1,27 @@
 #include <stdio.h>
 
+int wildcmp_recursive(char *s1, char *s2);
+
+/**
+ * match - Check if two strings match using wildcards.
+ * @s1: The first string to compare.
+ * @s2: The second string with wildcards.
+ *
+ * Return: 1 if the strings match, 0 otherwise.
+ */
+int match(char *s1, char *s2)
+{
+	if (*s1 == '\0' && *s2 == '\0')
+	{
+		return (1);
+	}
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		return (wildcmp_recursive(s1, s2));
+	}
+	return (0);
+}
+
 /**
  * wildcmp_recursive - Recursively compare two strings with wildcards.
  * @s1: The first string to compare.
@@ -15,18 +37,7 @@ int wildcmp_recursive(char *s1, char *s2)
 	}
 	if (*s2 == '*')
 	{
-		if (wildcmp_recursive(s1, s2 + 1))
-		{
-			return (1);
-		}
-		while (*s1 != '\0')
-		{
-			if (wildcmp_recursive(s1 + 1, s2))
-			{
-				return (1);
-			}
-			s1++;
-		}
+		return (match(s1, s2 + 1) || (*s1 != '\0' && wildcmp_recursive(s1 + 1, s2)));
 	}
 	if (*s1 == *s2)
 	{
@@ -44,5 +55,5 @@ int wildcmp_recursive(char *s1, char *s2)
  */
 int wildcmp(char *s1, char *s2)
 {
-	return (wildcmp_recursive(s1, s2));
+	return (match(s1, s2));
 }
