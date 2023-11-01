cf435395effa04d8e64f73291cf9fcb3bbd024e3
dnyamwamu <dnyamwamu@gmail.com>
2023-11-01 10:20:42 -0700

    Add

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 for success, 1 for error
+ */
+int main(int argc, char *argv[])
+{
+	int sum = 0;
+	int i, j, num;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		num = atoi(argv[i]);
+		if (num > 0)
+			sum += num;
+	}
+	printf("%d\n", sum);
+	return (0);
+}
