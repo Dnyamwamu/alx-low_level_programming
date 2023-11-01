3705fcce974899aabf84138900f8041b6cd59545
dnyamwamu <dnyamwamu@gmail.com>
2023-11-01 10:04:06 -0700

    Args

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,43 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
 
 /**
- * _putchar - writes a character to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
-}
-
-/**
- * main - Entry point
- * @argc: Number of command-line arguments
- * @argv: Array containing the command-line arguments
- *
- * Return: 0 (Success)
+ * main - Prints the number of arguments passed into the program
+ * @argc: The number of arguments
+ * @argv: An array of argument strings
+ * Return: 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
-	int count = argc - 1;
-	char digit;
-
-	if (count == 0)
-	{
-		_putchar('0');
-	}
-	else
-	{
-		while (count > 0)
-		{
-			digit = (count % 10) + '0';
-			_putchar(digit);
-			count /= 10;
-		}
-	}
-	_putchar('\n');
+	printf("%d\n", argc - 1);
 	return (0);
 }
