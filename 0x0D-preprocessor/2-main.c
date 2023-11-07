5875e22425f3547660e71f7c6dc16a7e2d710360
dnyamwamu <dnyamwamu@gmail.com>
2023-11-06 18:49:32 -0800

    main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,4 @@
 #include <stdio.h>
-#include <unistd.h>
-
-/**
- * _putchar - Writes a character to the standard output (stdout).
- *
- * @c: The character to be written.
- *
- * Return: On success, returns the character written. On error, returns -1.
- */
-int _putchar(char c);
 
 /**
  * main - Entry point
@@ -17,19 +7,6 @@ int _putchar(char c);
  */
 int main(void)
 {
-	const char *file = __FILE__;
-	int i = 0;
-
-	while (file[i] != '\0')
-	{
-		_putchar(file[i]);
-		i++;
-	}
-	_putchar('\n');
+	printf("%s\n", __FILE__);
 	return (0);
-}
-
-int _putchar(char c)
-{
-	return (write(1, &c, 1));
 }
