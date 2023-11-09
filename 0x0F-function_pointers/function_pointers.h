ec80686b3163cbaec13d6362a48d792f1a3a2052
dnyamwamu <dnyamwamu@gmail.com>
2023-11-09 10:54:41 -0800

    Array iterator

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -6,4 +6,7 @@ void print_name_uppercase(char *name);
 void print_name(char *name, void (*f)(char *));
 void print_upper(char *name);
 void print_lower(char *name);
+void print_elem(int elem);
+void print_elem_hex(int elem);
+void array_iterator(int *array, size_t size, void (*action)(int));
 #endif /* FUNCTION_POINTERS_H */
